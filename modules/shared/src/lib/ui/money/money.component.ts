8c3573dc32d5556b79c0582fed07fe06f0ea2b7b
hamze <hamze.mostafaei@gmail.com>
2024-10-13 02:22:49 +0330

    Sample number seperator directive added

--- modules/shared/src/lib/ui/money/money.component.ts
+++ modules/shared/src/lib/ui/money/money.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NzDescriptionsComponent, NzDescriptionsItemComponent } from 'ng-zorro-antd/descriptions';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzTooltipDirective } from 'ng-zorro-antd/tooltip';
+import { NzInputDirective } from 'ng-zorro-antd/input';
+import { SeparatorDirective } from '../separator-directive';
 
 @Component({
   selector: 'lib-money',
   standalone: true,
-  imports: [],
+  imports: [
+    NzDescriptionsComponent,
+    NzDescriptionsItemComponent,
+    FormsModule,
+    NzTooltipDirective,
+    NzInputDirective,
+    ReactiveFormsModule,
+    SeparatorDirective
+  ],
   templateUrl: './money.component.html',
   styleUrl: './money.component.css'
 })
-export class MoneyComponent implements OnInit {
-
-  ngOnInit(): void {
-    console.log('hamze', 'hamze');
-    console.log('Ghorbanali','ghorbanali');
-  }
-
+export class MoneyComponent {
 }
